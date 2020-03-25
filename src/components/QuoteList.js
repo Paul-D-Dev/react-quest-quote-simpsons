102237c44bbb3c7cc5a98651453035e7d0215903
Paul DAO <dao.van.paul@gmail.com>
2020-03-25 17:05:58 +0100

    add componentDidMount to call api request when init app

--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -2,47 +2,35 @@ import React from "react";
 import QuoteCard from './QuoteCard';
 import axios from 'axios';
 
-// An array of objects
-const initialQuote = {
-    quote:
-      "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
-    character: "Homer Simpson",
-    image:
-      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-  }
-
 class QuoteList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote : initialQuote
+      quote : null
     }
 
     this.getQuote = this.getQuote.bind(this);
   }
 
+  componentDidMount() {
+    this.getQuote();
+  }
+
   getQuote() {
-    this.setState({loading : true}, () => {
       axios.get(`https://simpsons-quotes-api.herokuapp.com/quotes`)
       .then(response => response.data)
       .then(data => {
         this.setState({
-          loading: false,
           quote: data[0]
         })
       })
-    })
   }
 
   render() {
-    const {loading} = this.state;
+    const initQuote = this.state.quote ? <QuoteCard quote={this.state.quote}/> : <p>Loading</p>
     return (
       <div>
-        {loading ? 
-        <p>Loading</p>
-          :
-        <QuoteCard quote={this.state.quote}/>
-        }
+        {initQuote}
         <button type="submit" onClick={this.getQuote}>Get quote</button>
       </div>
     )
