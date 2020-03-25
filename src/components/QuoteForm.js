b47a18d502e9fab0ed35dd85ba952adb02562942
Paul DAO <dao.van.paul@gmail.com>
2020-03-25 12:46:13 +0100

    QuoteForm : add max length value indicator

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style/QuoteForm.css'
 
+const MAX_LENGTH = 30;
 export default class QuoteForm extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {character : 'Homer Simpson'};
+        this.state = {character : ''};
 
         // this est bien associé à l'instance dans la méthode handleChange
         // sans ça this = undefined
@@ -21,20 +22,27 @@ export default class QuoteForm extends React.Component {
         event.preventDefaults();
     }
 
-
     render() {
+        const maximumReached = this.state.character.length >= MAX_LENGTH;
+        const nbRemaining = MAX_LENGTH - this.state.character.length;
+
       return (
         <form onSubmit={this.handleSubmit}
           className="QuoteForm"
         >
           <label htmlFor="character">Character:</label>
           <input
+            className= {maximumReached ? 'length-maximum-reached' : 'length-ok'}
             id="character"
             name="character"
             type="text"
             value={this.state.character}
-            onChange={this.handleChange}
+            onChange={this.handleChange}  
           />
+          <small className="remaining-characters">
+              {nbRemaining} remaining characters
+          </small>
+        
         </form>
       );
     }
